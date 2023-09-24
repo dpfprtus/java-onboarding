b33d8820ed50e666c5b44d61edafe52ccc8bd01a
suyong choi <dpfprtus@gmail.com>
2023-09-24 18:52:50 +0900

    feat: (Problem 2) 문자열 검증 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem2 {
+    private static final int MAX_LENGTH = 1000;
+    private static final int MIN_LENGTH = 1;
+    private static final char FIRST_LETTER = 'a';
+    private static final char LAST_LETTER = 'z';
+
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static void validateLength(String cryptogram) {
+        if (cryptogram.length() < MIN_LENGTH || cryptogram.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("허용되지 않은 길이의 문자열입니다.");
+        }
+    }
+    public static void validateIsLower(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char letter = cryptogram.charAt(i);
+            checkIsLower(letter);
+        }
+    }
+
+    public static void checkIsLower(char letter) {
+        if (letter < FIRST_LETTER || letter > LAST_LETTER) {
+            throw new IllegalArgumentException("소문자가 아닌 다른 값이 포함되어 있습니다.");
+        }
     }
 }
