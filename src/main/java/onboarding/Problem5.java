478fe436eda90de5029764b60c45ac6e95d87646
suyong choi <dpfprtus@gmail.com>
2023-09-26 19:11:04 +0900

    feat: (Problem 5) 지폐 및 동전이 몇개 필요한지 구하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> LIST_OF_MONERY_UNIT = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10,1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
         try {
             validateInputSize(money);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
-        return answer;
+
+        return calculateMoneyCount(money);
     }
 
-    private static
+    public static List<Integer> calculateMoneyCount(int money) {
+        List<Integer> moneyCount = new ArrayList<>();
+        for (Integer integer : LIST_OF_MONERY_UNIT) {
+            moneyCount.add(divide(money, integer));
+            money = restOfTheMoney(money, integer);
+        }
+        return moneyCount;
+    }
+
+    public static int divide(int money,int unit) {
+        return (money / unit);
+    }
+
+    public static int restOfTheMoney(int money, int unit) {
+        return (money % unit);
+    }
 
     public static void validateInputSize(int money) {
         if (money < 1 || money > 1000000) {
