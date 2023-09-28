7344e3255f02e1ed293ac7c7a7d17ae1144146c1
suyong choi <dpfprtus@gmail.com>
2023-09-28 11:10:42 +0900

    refactor: (Problem 7) 접근제어자 public -> private

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,24 +5,28 @@ import java.util.*;
 public class Problem7 {
     private static final int MAX_NUMBER_OF_PEOPLE_CAN_RECOMMEND = 5;
     private static Map<String, Set<String>> friendList = new HashMap<>();
+    private static Map<String, Integer> recommendScore = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         makeFriendList(friends);
         return answer;
     }
 
-    public static void makeFriendList(List<List<String>> friends) {
+
+
+
+    private static void makeFriendList(List<List<String>> friends) {
         for (List<String> friend : friends) {
             addFriendList(friend.get(0),friend.get(1));
             addFriendList(friend.get(1),friend.get(0));
         }
     }
 
-    public static boolean checkFriendListKey(String user) {
+    private static boolean checkFriendListKey(String user) {
         return friendList.containsKey(user);
     }
 
-    public static void addFriendList(String key,String value) {
+    private static void addFriendList(String key,String value) {
         if (checkFriendListKey(key)) {
             friendList.get(key).add(value);
             return;
